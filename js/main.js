2122171362b36746685b5bf3f3bc2e0d0c511889
Bayusukmoadji <bayu@Bayus-MacBook-Air.local>
2025-06-29 21:52:37 +0700

    firs

--- js/main.js
+++ js/main.js
@@ -1,60 +1,78 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner(0);
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner(0);
 
+  // Initiate the wowjs
+  new WOW().init();
 
-    // Fixed Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
-        } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-300px');
-        }
-    });
-
-
-    // Smooth scrolling on the navbar links
-    $(".navbar-nav a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 90
-            }, 1500, 'easeInOutExpo');
-            
-            if ($(this).parents('.navbar-nav').length) {
-                $('.navbar-nav .active').removeClass('active');
-                $(this).closest('a').addClass('active');
-            }
-        }
-    });
-    
-    
-   // Back to top button
-   $(window).scroll(function () {
+  // Fixed Navbar
+  $(window).scroll(function () {
     if ($(this).scrollTop() > 300) {
-        $('.back-to-top').fadeIn('slow');
+      $(".sticky-top").addClass("shadow-sm").css("top", "0px");
     } else {
-        $('.back-to-top').fadeOut('slow');
+      $(".sticky-top").removeClass("shadow-sm").css("top", "-300px");
     }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    }); 
+  });
 
+  // Smooth scrolling on the navbar links
+  $(".navbar-nav a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      $("html, body").animate(
+        {
+          scrollTop: $(this.hash).offset().top - 90,
+        },
+        1500,
+        "easeInOutExpo"
+      );
+
+      if ($(this).parents(".navbar-nav").length) {
+        $(".navbar-nav .active").removeClass("active");
+        $(this).closest("a").addClass("active");
+      }
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  window.addEventListener("load", function () {
+    const music = document.getElementById("background_music");
+
+    // Mencoba memutar musik
+    var playPromise = music.play();
+
+    if (playPromise !== undefined) {
+      playPromise
+        .then((_) => {
+          // Autoplay berhasil (sangat jarang terjadi untuk audio dengan suara).
+          console.log("Autoplay musik dimulai.");
+        })
+        .catch((error) => {
+          // Autoplay diblokir oleh browser.
+          console.error("Autoplay musik diblokir oleh browser:", error);
+          // Di sini Anda bisa menampilkan tombol play secara manual jika mau.
+        });
+    }
+  });
 })(jQuery);
-
